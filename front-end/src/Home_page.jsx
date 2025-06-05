c814f4d2b5695e48de30e5a0cc9285085cad6e0f
Moali <moh.ali6066@gmail.com>
2025-06-05 03:32:26 +0300

    completed the about part

--- front-end/src/Home_page.jsx
+++ front-end/src/Home_page.jsx
@@ -7,6 +7,7 @@ import slide_2 from './assets/slide10.jpg'
 import slide_4 from './assets/slide_4.jpg'
 import slide6 from './assets/slide11.jpg'
 import slide7 from './assets/slide7.jpg'
+import better from './assets/better_furniture.png'
 
 function Home_page() {
 
@@ -63,13 +64,25 @@ function Home_page() {
 
         
         <div className='section_3'>
-            <h1 className='first_part'>About US</h1>
-            <div className='contact_us_tabel'>
-                <h1>How to reach me?</h1>
-                <h3>Personal email?</h3><p><a href='https://mail.google.com/mail/u/0/#inbox?compose=CllgCJqXxbvQwXLSXBnGdKFmrNDdbWVVdStTprnCBRTLGCxpczJcPswcpQmsfxMKzJJgLJHCPmL'>moh.ali6066@gmail.com</a></p>
-                <h3>University email</h3><p><a href='https://mail.google.com/mail/u/0/#inbox?compose=CllgCJqXxbvQwXLSXBnGdKFmrNDdbWVVdStTprnCBRTLGCxpczJcPswcpQmsfxMKzJJgLJHCPmL'>mohamed.alysoliman@student.guc.edu.eg</a></p>
-                <h3>Linked in Account</h3><p><a href='https://www.linkedin.com/in/mohamed-aly-6b7a32200/'>Linked In</a></p>
-            </div>
+                <div>  
+                  <h1 className='first_part'>About US</h1>
+                    <div className='contact_us_tabel'>
+                      <h1>How to reach me?</h1>
+                      <h3>Personal email?</h3><p><a href='https://mail.google.com/mail/u/0/#inbox?compose=CllgCJqXxbvQwXLSXBnGdKFmrNDdbWVVdStTprnCBRTLGCxpczJcPswcpQmsfxMKzJJgLJHCPmL'>moh.ali6066@gmail.com</a></p>
+                      <h3>University email</h3><p><a href='https://mail.google.com/mail/u/0/#inbox?compose=CllgCJqXxbvQwXLSXBnGdKFmrNDdbWVVdStTprnCBRTLGCxpczJcPswcpQmsfxMKzJJgLJHCPmL'>mohamed.alysoliman@student.guc.edu.eg</a></p>
+                      <h3>Linked in Account</h3><p><a href='https://www.linkedin.com/in/mohamed-aly-6b7a32200/'>Linked In</a></p>
+                    <div/>
+                  </div>
+                  <div className='second_part'>
+                     <h1 style={{color: 'green'}}>About me</h1>I am engineering student at the German university in cairo. Passionate about Learning new technologies.
+                      I did more than one intership in different companies Like: <li><a href='https://iskraemeco.com/'><b>Elsewedy electric</b></a> as a software tester 
+                      engineer for two weeks</li> <li>3 Months in <b><a href='https://www.orange-business.com/en'>Orange business service</a></b> as a cloud engineer</li>  
+                      <li>1 month in <b><a href='https://www.linkedin.com/company/edgeconn/posts/?feedView=all'>Edgeconn</a></b> as a network engineer</li>
+                  </div>
+                  <div>
+                    <img src={better} className="better_image"/>
+                  </div>
+                </div>
 
         </div>
     </>
