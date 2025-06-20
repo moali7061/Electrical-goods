13fe14c5772c6547453be98662cea2f85df72081
Moali <moh.ali6066@gmail.com>
2025-06-20 10:51:34 +0300

    frontend of sign_up done

---
+++ front-end/src/sign_up.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+
+function Signup(){
+
+const navigate = useNavigate();
+ 
+  
+
+  const [email, setEmail] = useState("");
+  function setting_the_email(event){
+    setEmail(event.target.value);
+  }
+
+  const [password, setPassword] = useState("");
+  function setting_the_pssword(event){
+    setPassword(event.target.value);
+  }
+
+  const [username, setUsername]=useState("");
+  function setting_the_username(event){
+        setUsername(event.target.value);
+  }
+
+    return(
+        <div className="login_page">
+            <div className="login_part1">
+                <h1 style={{fontSize: '200px', marginTop: '200px'}}>Sign Up</h1>
+                <div className="inputing_fields">
+                    <input style={{marginTop: '50px'}} placeholder="username" value={username} onChange={setting_the_username} required></input>
+
+                    <input placeholder="email" value={email} onChange={setting_the_email} required></input>
+
+                    <input placeholder="password" value={password} onChange={setting_the_pssword} required ></input>
+
+                    <div className = 'two_buttons'>
+                        <button onClick={signing_up_user} style={{width:'300px', border:'solid black 2px', borderRadius: '20px'}} >Register</button>
+                        <button onClick={signing_up_user} style={{width:'300px', border:'solid black 2px', borderRadius: '20px'}} >go to home page</button>
+                    </div>
+                </div>
+            </div>
+
+
+            <div className="login_part2">
+            </div>
+        </div>
+    );
+}
+
+export default Signup;
