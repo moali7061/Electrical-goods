58da1a804fb9424a17bf42a0195233e428849223
Moali <moh.ali6066@gmail.com>
2025-10-12 12:52:11 +0300

    adding the css for the buttons of the changing password webpage and adding usestate for the change password and go to home page website in the change_password webpage

--- front-end/src/change_password.jsx
+++ front-end/src/change_password.jsx
@@ -1,14 +1,27 @@
-
+import { useState } from "react";
 
 
 function Change_password(){
 
+    const [email, setEmail] = useState("");
+    function setting_the_email (event){
+        setEmail(event.target.value);
+    }
+
+    const [password, setPassword] = useState("");
+    function setting_the_password (event){
+        setPassword(event.target.value);
+    }
+
     return(
-        <div className="change_password " >
+        <div className="change_password" >
             <div className="inputing_fields">
-                <input placeholder="email"></input>
-                <input placeholder="password"></input>
-                <button className="Home_page_button">change</button>
+                <input placeholder="email" value={email} onChange={setting_the_email} required></input>
+                <input placeholder="password" value={password} onChange={setting_the_password} required></input>
+                <div className="buttons_in_row">
+                    <button className="Home_page_button">change password</button>
+                    <button className="Home_page_button">go to home</button>
+                </div>
             </div>
         </div>
     );
