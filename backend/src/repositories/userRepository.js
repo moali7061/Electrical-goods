3ad06e37c093630cc4bba8a1cf7f95035fb5b4ba
Moali <moh.ali6066@gmail.com>
2025-10-29 22:57:38 +0300

    adding the change password in the userRepository

--- backend/src/repositories/userRepository.js
+++ backend/src/repositories/userRepository.js
@@ -5,3 +5,6 @@ export async function findByEmail(email){
     return result.rows[0];
 }
 
+export async function changePassword(email, hashedpassword){
+    await db.query('UPDATE students SET password = $1 where email = $2',[hashedpassword, email]);
+}
