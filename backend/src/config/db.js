0f5e26ff1393f325d7fad1c3e1cd3ccf4410b75d
Moali <moh.ali6066@gmail.com>
2025-10-29 22:23:33 +0300

    adding src folder for the backend to make new architecture for the backend

---
+++ backend/src/config/db.js
@@ -0,0 +1,14 @@
+import {client} from 'pg';
+import pg from 'pg';
+
+const db = new pg.Client({
+    user: 'postgres',
+    password: 'ZoZoZoZo1234',
+    host: 'localhost',
+    port: 5432,
+    database: 'school'
+})
+
+db.connect().then(console.log("connected to the database"));
+
+export default db;
---
+++ backend/src/repositories/userRepository.js
@@ -0,0 +1,6 @@
+import db from '../config/db.js';
+
+export async function findByEmail(email){
+    const result = await db.query('select * from sudents where email=$1',[email]);
+    return result.rows[0];
+}
