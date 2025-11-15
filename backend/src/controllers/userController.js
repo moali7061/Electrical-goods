530e3937101f2af31e5e48b801189477fae34a2e
Moali <moh.ali6066@gmail.com>
2025-11-15 08:30:20 +0200

    added the sending emails when the user sign up or log in successfully

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,11 +1,35 @@
 import {registerUser,change_password, login_user} from '../services/userService.js'
 import { findByEmail } from '../repositories/userRepository.js';
+import nodemailer from 'nodemailer';
 
 export const signUpUser = async(req, res)=>{
     try{
         const {username, email, password} = req.body;
         const result = await registerUser(username, email, password);
-        
+        if(result =="created"){
+            const transporter = nodemailer.createTransport({
+                service: 'gmail',
+                auth:{
+                    user: 'betterFurniturebetterfuture@gmail.com',
+                    pass: 'yjnp bzhj gobe lgfn'
+                }
+            });
+
+            const subject = "welcome to better furniture better future";
+            const mailOptions = {
+                from: 'betterFurniturebetterfuture@gmail.com',
+                to: email,
+                subject: subject,
+                text: 'this is a test email. your account is created successfully'
+            };
+            transporter.sendMail(mailOptions, function(error, info){
+                if(error){
+                    console.log("emailerror: ", error)
+                }else{
+                    console.log("Email sent:", info.response);
+                }
+            })
+        }
         res.status(200).json({message: result});
     }
     catch(err){
@@ -34,8 +58,31 @@ export const login = async(req, res)=>{
         if (result==="correct") {
              req.session.userId = user.id;
              console.log("request session id = "+ req.session.userId);
-             req.session.username = user.username;    
+             req.session.username = user.username;
+             const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth:{
+                user: 'betterFurniturebetterfuture@gmail.com',
+                pass: 'yjnp bzhj gobe lgfn'
+            }
+        });
+
+        const subject = "welcome to better furniture better future";
+        const mailOptions = {
+            from: 'betterFurniturebetterfuture@gmail.com',
+            to: email,
+            subject: subject,
+            text: 'your account logged in successfully'
+        };
+        transporter.sendMail(mailOptions, function(error, info){
+            if(error){
+                console.log("emailerror: ", error)
+            }else{
+                console.log("Email sent:", info.response);
+            }
+        })    
         }
+        
         res.status(200).json({message: result});
     }catch(err){
         res.status(500).json({message: err.message});
