ecf819b7a1023cc6f0792865bfd6b9f696223339
Moali <moh.ali6066@gmail.com>
2026-04-25 03:16:13 +0300

    advanced UI for carousel that is used in the front-end

--- front-end/src/components/carousel/carousel.jsx
+++ front-end/src/components/carousel/carousel.jsx
@@ -1,70 +1,64 @@
-import {BsArrowLeftCircleFill, BsArrowRightCircleFill} from "react-icons/bs"
 import { useState } from "react";
-import slide_1 from "../../assets/test1.png"
-import slide_2 from "../../assets/test2.png"
-import slide_3 from "../../assets/test3.png"
-import slide_4 from "../../assets/test4.png"
-import slide_5 from "../../assets/test5.png"
-import slide_6 from "../../assets/test6.png"
-import slide_7 from "../../assets/test7.png"
-import slide_8 from "../../assets/test8.png"
+import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
+import slide_1 from "../../assets/test1.png";
+import slide_2 from "../../assets/test2.png";
+import slide_3 from "../../assets/test3.png";
+import slide_4 from "../../assets/test4.png";
+import slide_5 from "../../assets/test5.png";
+import slide_6 from "../../assets/test6.png";
+import slide_7 from "../../assets/test7.png";
+import slide_8 from "../../assets/test8.png";
+import "./carousel.css";
 
-import "./carousel.css"
+const list = [
+  { src: slide_1, alt: "slide 1" },
+  { src: slide_2, alt: "slide 2" },
+  { src: slide_3, alt: "slide 3" },
+  { src: slide_4, alt: "slide 4" },
+  { src: slide_5, alt: "slide 5" },
+  { src: slide_6, alt: "slide 6" },
+  { src: slide_7, alt: "slide 7" },
+  { src: slide_8, alt: "slide 8" },
+];
 
-export function Carousel  (props){
-    const list = [
-        {
-            src: slide_1,
-            alt: ""
-        },
-        {
-            src: slide_2,
-            alt: ""
-        },
-        {
-            src: slide_3,
-            alt: ""
-        },
-        {
-            src: slide_4,
-            alt: ""
-        },
-        {
-            src: slide_5,
-            alt: ""
-        },
-        {
-            src: slide_6,
-            alt: ""
-        },
-        {
-            src: slide_7,
-            alt: ""
-        },
-        {
-            src: slide_8,
-            alt: ""
-        }
+export function Carousel() {
+  const [slide, setSlide] = useState(0);
 
-    ]
-    const [slide, setSlide] = useState(0);
-    const nextslide=()=>{
-        setSlide(slide===list.length -1? 0: slide +1);
-    }
-    const prevslide=()=>{
-        setSlide(slide===0? data.length-1 : slide - 1);
-    }
-    return(
+  const nextSlide = () => setSlide((prev) => (prev === list.length - 1 ? 0 : prev + 1));
+  const prevSlide = () => setSlide((prev) => (prev === 0 ? list.length - 1 : prev - 1));
+
+  return (
     <div className="carousel">
-        <BsArrowLeftCircleFill className="arrow arrow-left" onClick={prevslide}/>
-        {list.map((e, idx)=>{
-            return(<img src={e.src} alt={e.alt} key={idx} className={slide=== idx? "slide": "slide slide-hidden"}/>);
-        })}
-        <BsArrowRightCircleFill className="arrow arrow-right" onClick={nextslide}/>
-        <span className="indicators">
-            {list.map((_, idx)=>{
-                return <button key={idx} onClick={null} className={slide === idx? "indicator": "indicator-inactive"}></button>
-            })}
-        </span>
-    </div>);
+
+      {list.map((e, idx) => (
+        <img
+          src={e.src}
+          alt={e.alt}
+          key={idx}
+          className={`carousel__slide${slide === idx ? " carousel__slide--active" : ""}`}
+        />
+      ))}
+
+      <button className="carousel__arrow carousel__arrow--left" onClick={prevSlide} aria-label="Previous">
+        <BsArrowLeftCircleFill />
+      </button>
+      <button className="carousel__arrow carousel__arrow--right" onClick={nextSlide} aria-label="Next">
+        <BsArrowRightCircleFill />
+      </button>
+
+      <span className="carousel__counter">{slide + 1} / {list.length}</span>
+
+      <div className="carousel__dots">
+        {list.map((_, idx) => (
+          <button
+            key={idx}
+            onClick={() => setSlide(idx)}
+            className={`carousel__dot${slide === idx ? " carousel__dot--active" : ""}`}
+            aria-label={`Go to slide ${idx + 1}`}
+          />
+        ))}
+      </div>
+
+    </div>
+  );
 }
