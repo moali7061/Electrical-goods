5461ed67c22626064dfa316c89b226280c0450d2
Moali <moh.ali6066@gmail.com>
2026-04-25 03:20:39 +0300

    modified the css for the selection cards that is displayed in the home page

--- front-end/src/components/selection_card/selection_card1.jsx
+++ front-end/src/components/selection_card/selection_card1.jsx
@@ -1,27 +1,28 @@
-import "./selection_card.css"
+import "./selection_card.css";
 import { useNavigate } from "react-router-dom";
-import Navbar from "../navbar/Navbar";
 
-function Selection_card1(props){
-
+function Selection_card1(props) {
     const navigate = useNavigate();
-    
+
     const go_to_list = () => {
         if (typeof props.direction === "string") {
             navigate(props.direction);
         } else {
             navigate(props.direction.path, {
-            state: props.direction.state
+                state: props.direction.state
             });
         }
     };
 
-    return(
-        <div className="selection_card">
-            <Navbar type="home"/>
-            <img src={props.image} alt="selection" style={{width: '190px', height:'150px'}} direction="/listing"/>
-            <hr/>
-            <button onClick={go_to_list}>select</button>
+    return (
+        <div className="selection_card" onClick={go_to_list}>
+            <img
+                src={props.image}
+                alt="selection"
+                className="selection_card__img"
+            />
+            <hr className="selection_card__divider" />
+            <button className="selection_card__btn">Select</button>
         </div>
     );
 }
